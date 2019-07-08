9c0be4f1a3ff090a0d87526bef8c1ecd20b9d856
Denis Tereschenko <terdenan@gmail.com>
2019-07-08 23:35:28 +0300

    Удалены лишние файлы

--- src/actions/index.js
+++
@@ -1,5 +0,0 @@
-export const SET_ACTIVE_PANEL = 'SET_ACTIVE_PANEL';
-export const setActivePanel = panel => ({
-  type: SET_ACTIVE_PANEL,
-  activePanel: panel,
-});
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import activePanel from './activePanel';
-
-export default combineReducers({
-  activePanel,
-});
